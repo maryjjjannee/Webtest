d0dfee8c11cdcfad9aa3756b0bbb4331d6949e75
Jane Onwanya <123064279+maryjjjannee@users.noreply.github.com>
2025-05-30 19:35:32 +0700

    Refactor app.js to fetch course data and display it in index.html

--- app.js
+++ app.js
@@ -1,24 +1,20 @@
-import {useEffect, useState} from 'react';
-
-export default function App() {
-  const [data, setData] = useState("");
-  const getData = async () => {
-    try {
-      const resp = await fetch('https://api.sampleapis.com/coffee/hot');
-      const json = await resp.json();
-      setData(json);
-    } catch (err) {
-      setData(err.message);
+function appendData(data) {
+    var mainContainer = document.getElementById('myData');
+    for (var i = 0; i < data.length; i++) {
+        var div = document.createElement('div');
+        div.innerHTML = 'CourseID: ' + data[i].id + ' ' + data[i].name + ' ' + data[i].price + ' ' + data[i].instructor;
+        mainContainer.appendChild(div);
     }
-  }
-
-  useEffect(() => {
-    getData();
-  }, []);
-
-  return (
-    <pre>
-      {JSON.stringify(data, null, 2)}
-    </pre>
-  )
 }
+
+fetch('http://localhost:8080/course')
+  .then(res => {
+    if (!res.ok) throw new Error('Network response was not ok');
+    return res.json();
+  })
+  .then(data => {
+    appendData(data);
+  })
+  .catch(err => {
+    console.error('Fetch error:', err);
+  });
