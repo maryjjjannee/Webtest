e7569354f55311377784e84e52a04de51cd24451
Jane Onwanya <123064279+maryjjjannee@users.noreply.github.com>
2025-06-04 15:41:59 +0700

    Update fetch URL to use secure endpoint for course data
    Update fetch URL to use secure endpoint for course data

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ function appendData(data) {
     }
 }
 
-fetch('http://localhost:8080/course')
+fetch('https://stale-lies-write.loca.lt/course')
   .then(res => {
     if (!res.ok) throw new Error('Network response was not ok');
     return res.json();
